b1b30607642df75ce007f6fc42234deb8c884b83
jia1551189282 <33778830+jia1551189282@users.noreply.github.com>
2020-02-02 13:03:40 +0800

    首页轮播图

---
+++ foodie-dev-common/src/main/java/com/imooc/enums/YesOrNo.java
@@ -0,0 +1,20 @@
+package com.imooc.enums;
+
+/**
+ * 是否 枚举类
+ * 0 表示否
+ * 1 表示是
+ */
+public enum YesOrNo {
+    NO(0,"否"),
+    YES(1,"是");
+
+    public final Integer type;
+    public final String value;
+
+    YesOrNo(Integer type,String value){
+        this.type = type;
+        this.value = value;
+    }
+
+}
